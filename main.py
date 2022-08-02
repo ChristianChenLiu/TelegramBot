37aa6f2df0dda31255b8961403563d0daf297c94
Christian Chen Liu <Christian.chenliu@mail.utoronto.ca>
2022-08-02 17:44:46 -0400

    Bot initialization

---
+++ main.py
@@ -0,0 +1,13 @@
+import os, telebot
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
+bot = telebot.TeleBot(API_KEY)
+
+@bot.message_handler(commands=['Pop'])
+def weirdo(message):
+    bot.reply_to(message, "Weeb!")
+
+bot.polling()
